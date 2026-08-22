f59924e81e47d4ab434e55e058962a6fd388035a
Elias Zolotas <ilizol@yahoo.gr>
2026-08-22 12:12:22 +0300

    Add SBMuFL support. Update 11.

--- docs/data/sbmufl/kontakion_prophet_elias.js
+++ docs/data/sbmufl/kontakion_prophet_elias.js
@@ -11,308 +11,308 @@ neumes.push(
     { "tl": "Ἦχος" },
     { "t": " " }, // SPACE
     //###### MARTYRIA ######
-    { "m": "\uE2A8" },
+    { "m": "\uE2A8" }, // U+E2A8 = modeSecond
     { "t": " " }, // SPACE
     { "tl": "Δι" },
     { "t": "  " }, // SPACE
     //###### MARTYRIA ######
-    { "mf": "\uE19A" },
+    { "mf": "\uE19A" }, // U+E19A = fthoraSoftChromaticDiAbove
     { "br": "ln2" }, // LINE BREAK
     { "br": "ln2" }, // LINE BREAK
     //{"br":"ln"}, // LINE BREAK
     //###### DROPCAP ######
     { "d": "Π" },
     {
-        "n": "\uE024",
+        "n": "\uE024", // U+E024 = elafron
         "i": "Μ",
         "l": "ρο"
     },
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE000",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE000", // U+E000 = ison
         "l": "φη"
     },
     {
-        "n": "\uE082",
+        "n": "\uE082", // U+E082 = oligonKentimataBelow
         "l": "η"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "i": "Δ",
         "l": "τα"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "και"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "προ"
     },
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE041",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE041", // U+E041 = petasti
         "r": "3",
         "l": "ο"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "πτα"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "των",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "με"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "γα"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "λουρ"
     },
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "γι"
     },
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE001",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE001", // U+E001 = oligon
         "r": "3",
         "l": "ων"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "του"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "Θε",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     {
-        "n": "\uE041",
+        "n": "\uE041", // U+E041 = petasti
         "l": "ου"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "Η"
     },
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "λι"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "α"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "με"
     },
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "γα"
     },
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE003\uE0A1",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE003\uE0A1", // U+E003 = oligonKentimaBelow, U+E0A1 = psifiston
         "r": "3",
         "l": "λω"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "ω"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "νυ",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     {
-        "n": "\uE021\uE0D0",
+        "n": "\uE021\uE0D0", // U+E021 = apostrofos, U+E0D0 = klasmaAbove
         "l": "με",
         //###### MARTYRIA ######
-        "m": "\uE13C\uE159"
+        "m": "\uE13C\uE159" // U+E13C = martyriaNoteDi, U+E159 = martyriaSoftChromaticDiBelow
     },
     {
-        "n": "\uE024",
+        "n": "\uE024", // U+E024 = elafron
         "i": "Μ",
         "l": "ο"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "τω"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "φθεγ"
     },
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "i": "Δ",
         "l": "μα"
     },
     {
-        "n": "\uE041",
+        "n": "\uE041", // U+E041 = petasti
         "l": "τι"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "σου"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE001",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE001", // U+E001 = oligon
         "r": "3",
         "l": "στη"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "σας"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "τα",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "υ"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "δα"
     },
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE041",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE041", // U+E041 = petasti
         "r": "3",
         "l": "τορ"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "ρυ"
     },
     {
-        "n": "\uE000",
+        "n": "\uE000", // U+E000 = ison
         "l": "τα",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE040",
+        "n": "\uE040", // U+E040 = petastiIson
         "iu": "Β",
         "l": "νε"
     },
     {
-        "n": "\uE022",
+        "n": "\uE022", // U+E022 = apostrofosSyndesmos
         "l": "ε",
-        "c": "\uE0F0"
+        "c": "\uE0F0" // U+E0F0 = gorgonAbove
     },
     {
-        "n": "\uE084",
-        "c": "\uE0F0",
+        "n": "\uE084", // U+E084 = oligonIsonKentimata
+        "c": "\uE0F0", // U+E0F0 = gorgonAbove
         "l": "ε"
     },
     {
-        "v": "\uE0A0", //TODO change
-        "n": "\uE000",
+        "v": "\uE0A0", // U+E0A0 = vareia
+        "n": "\uE000", // U+E000 = ison
         "l": "ε",
-        "n2": "\uE021",
-        "c2": "\uE0F0",
+        "n2": "\uE021", // U+E021 = apostrofos
+        "c2": "\uE0F0", // U+E0F0 = gorgonAbove
         "l2": "ε"
     },
     {
-        "n": "\uE000\uE0D0",
+        "n": "\uE000\uE0D0", // U+E000 = ison, U+E0D0 = klasmaAbove
         "l": "φη",
         //###### MARTYRIA ######
-        "m": "\uE13A\uE159"
+        "m": "\uE13A\uE159" // U+E13A = martyriaNoteVou, U+E159 = martyriaSoftChromaticDiBelow
     },
     {
-        "n": "\uE003\uE0A1",
+        "n": "\uE003\uE0A1", // U+E003 = oligonKentimaBelow, U+E0A1 = psifiston
         "i": "Μ",
         "l": "πρε"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": " σβευ"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "   ε"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "υ"
     },
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "περ"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "η"
     },
     {
-        "n": "\uE041",
+        "n": "\uE041", // U+E041 = petasti
         "l": "μων"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "τον"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "nb": "\uE210", // DIASTOLE
-        "n": "\uE001",
+        "nb": "\uE210", // DIASTOLE, U+E210 = barlineSingle
+        "n": "\uE001", // U+E001 = oligon
         "r": "3",
         "l": "μο"
     },
     {
-        "n": "\uE024",
+        "n": "\uE024", // U+E024 = elafron
         "l": "νον"
     },
     { "br": "wd" }, // WORD BREAK
     {
-        "n": "\uE001",
+        "n": "\uE001", // U+E001 = oligon
         "l": "Φι",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     {
-        "n": "\uE083",
+        "n": "\uE083", // U+E083 = oligonKentimataAbove
         "r": "3",
         "l": "λαν"
     },
     {
-        "n": "\uE021",
+        "n": "\uE021", // U+E021 = apostrofos
         "l": "θρω",
-        "n2": "\uE210" // DIASTOLE
+        "n2": "\uE210" // DIASTOLE, U+E210 = barlineSingle
     },
     {
-        "n": "\uE000\uE0D3",
+        "n": "\uE000\uE0D3", // U+E000 = ison, U+E0D3 = dipli
         "l": "πον"
     }
 
